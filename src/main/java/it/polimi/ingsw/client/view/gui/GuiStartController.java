d1240faf7b2215ea63c5ef96e53e7789608f26c6
umbertocolangelo <umberto.colangelo@mail.polimi.it>
2022-05-18 11:24:01 +0200

    gui start demo

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/GuiStartController.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.client.view.gui;
+
+import javafx.scene.input.MouseEvent;
+
+public class GuiStartController {
+
+    public void onClickEvent(MouseEvent mouseEvent) {
+        //goToLogin
+    }
+
+}
