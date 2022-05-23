bc13c695cb5a98006f5d06cf82504e60a0995403
umbertocolangelo <umberto.colangelo@mail.polimi.it>
2022-05-23 17:20:47 +0200

    loading scene

---
+++ src/main/java/it/polimi/ingsw/client/view/gui/controllers/GuiChooseWizardAndColorController.java
@@ -0,0 +1,71 @@
+package it.polimi.ingsw.client.view.gui.controllers;
+
+import javafx.scene.input.MouseEvent;
+
+public class GuiChooseWizardAndColorController {
+
+    /**
+     * click on wizard1
+     * @param mouseEvent
+     */
+    public void onWizardClick1(MouseEvent mouseEvent) {
+        //appear next button (?)
+    }
+
+    /**
+     * click on wizard2
+     * @param mouseEvent
+     */
+    public void onWizardClick2(MouseEvent mouseEvent) {
+        //appear next button (?)
+    }
+
+    /**
+     * click on wizard3
+     * @param mouseEvent
+     */
+    public void onWizardClick3(MouseEvent mouseEvent) {
+        //appear next button (?)
+    }
+
+    /**
+     * click on wizard4
+     * @param mouseEvent
+     */
+    public void onWizardClick4(MouseEvent mouseEvent) {
+        //appear next button (?)
+    }
+
+    /**
+     * click on next button
+     * @param mouseEvent
+     */
+    public void onNextClick(MouseEvent mouseEvent) {
+
+    }
+
+    /**
+     * click on color white
+     * @param mouseEvent
+     */
+    public void onColorClick1(MouseEvent mouseEvent) {
+
+    }
+
+    /**
+     * click on color black
+     * @param mouseEvent
+     */
+    public void onColorClick2(MouseEvent mouseEvent) {
+
+    }
+
+    /**
+     * click on color grey
+     * @param mouseEvent
+     */
+    public void onColorClick3(MouseEvent mouseEvent) {
+
+    }
+
+}
--- src/main/java/it/polimi/ingsw/client/view/gui/controllers/GuiLoginController.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/controllers/GuiLoginController.java
@@ -20,7 +20,7 @@ public class GuiLoginController {
      */
     public void onClickEvent(MouseEvent mouseEvent) throws IOException {
         stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/wizard-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/deckandcolor-view.fxml"));
         scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setScene(scene);
         stage.show();
