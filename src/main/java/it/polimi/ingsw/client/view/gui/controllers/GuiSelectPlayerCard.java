10fd0453623c5b2bc8168c7b33a24f9ea24d6745
vittorio <vittorio.laferla@mail.polimi.it>
2022-06-07 12:41:52 +0200

    FInishing expertCArd on GUI

--- src/main/java/it/polimi/ingsw/client/view/gui/controllers/GuiSelectPlayerCard.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/controllers/GuiSelectPlayerCard.java
@@ -5,13 +5,22 @@ import it.polimi.ingsw.message.PlayExpertCard;
 import it.polimi.ingsw.model.Game;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GuiSelectPlayerCard implements Initializable {
+
+    /**
+     *
+     */
+    @FXML
+    AnchorPane scenePane;
 
     /**
      *
@@ -70,4 +79,23 @@ public class GuiSelectPlayerCard implements Initializable {
     public void clickPlayer1(MouseEvent mouseEvent) {
         player=game.getPlayerList().get(1).getId();
     }
+
+    public void resize(Stage stage, Scene scene) {
+        double height = stage.getHeight();
+        double width = stage.getWidth();
+
+        stage.setMinHeight(450);
+        stage.setMinWidth(800);
+
+        //scenePane.translateXProperty().bind(scene.widthProperty().subtract(scenePane.widthProperty().divide(2)));
+        //scenePane.translateYProperty().bind(scene.heightProperty().subtract(scenePane.heightProperty().divide(2)));
+
+        stage.widthProperty().addListener((obs, oldVal, newVal) -> {
+            double scaleX = newVal.doubleValue() / width;
+            scenePane.setScaleX(scaleX);
+            scenePane.setTranslateX(scenePane.getTranslateX() + (newVal.doubleValue() - oldVal.doubleValue()) / 2);
+            //scenePane.setCenterShape(true);
+        });
+    }
+
 }
