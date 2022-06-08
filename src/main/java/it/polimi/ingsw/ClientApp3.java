64358477f8e919867d17dd94bf2f47ff7be55af1
vittorio <vittorio.laferla@mail.polimi.it>
2022-06-08 02:11:02 +0200

    Implementig lobby

--- src/main/java/it/polimi/ingsw/ClientApp3.java
+++ src/main/java/it/polimi/ingsw/ClientApp3.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class ClientApp3 {
 
     public  static void main(String[] args) {
-        Client client = new Client("localhost", 65432);
+        Client client = new Client("192.168.1.130", 65432);
         try{
             client.run();
         }catch (IOException e){
