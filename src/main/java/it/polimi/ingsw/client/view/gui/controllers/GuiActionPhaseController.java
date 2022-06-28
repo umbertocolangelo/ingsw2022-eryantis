670d006b1488d6840cb74806179112274e5973b8
RobertoCialini <roberto.cialini@mail.polimi.it>
2022-06-28 19:18:53 +0200

    minor fix

--- src/main/java/it/polimi/ingsw/client/view/gui/controllers/GuiActionPhaseController.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/controllers/GuiActionPhaseController.java
@@ -4176,18 +4176,25 @@ public class GuiActionPhaseController implements Initializable {
      */
     public void showLoading() throws IOException{
         stage = GUIController.getInstance().getStage();
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/loading-view.fxml"));
-        scene = new Scene(fxmlLoader.load());
+        double x = stage.getX();
+        double y = stage.getY();
         double width = stage.getWidth();
         double height = stage.getHeight();
+        stage = new Stage();
+        GUIController.getInstance().getStage().close();
+        GUIController.setStage(stage);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/loading-view.fxml"));
+        scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
-        stage.setWidth(width);
-        stage.setHeight(height);
+        stage.setTitle("Eriantys");
         stage.show();
         GuiLoadingController loadingController = fxmlLoader.getController();
         loadingController.resize(stage);
-
         loadingController.setWaitText();
+        stage.setWidth(width);
+        stage.setHeight(height);
+        stage.setX(x);
+        stage.setY(y);
     }
 
     /**
