db63def5f91eea69b6ab56d8121fa1cec0449c15
RobertoCialini <roberto.cialini@mail.polimi.it>
2022-06-28 16:56:24 +0200

    Fixed GUI resize

--- src/main/java/it/polimi/ingsw/client/view/gui/controllers/GuiConnectionLost.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/controllers/GuiConnectionLost.java
@@ -26,40 +26,48 @@ public class GuiConnectionLost {
     private Scene scene;
 
     public void connectionLost() throws IOException {
+        stage = GUIController.getInstance().getStage();
+        double x = stage.getX();
+        double y = stage.getY();
+        double width = stage.getWidth();
+        double height = stage.getHeight();
         stage = new Stage();
         GUIController.getInstance().getStage().close();
         GUIController.setStage(stage);
-
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/connectionLost-view.fxml"));
-        scene = new Scene(fxmlLoader.load(), 1280, 720);
+        scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
+        stage.setTitle("Eriantys");
         stage.show();
-
         GuiConnectionLost controller = fxmlLoader.getController();
         controller.resize(stage);
+        stage.setWidth(width);
+        stage.setHeight(height);
+        stage.setX(x);
+        stage.setY(y);
     }
 
     /**
      * Window resize
      */
     public void resize(Stage stage) {
-        double height = stage.getHeight();
-        double width = stage.getWidth();
-
+        double height = stage.getScene().getHeight();
+        double width = stage.getScene().getWidth();
         //stage min sizes
         stage.setMinHeight(450);
         stage.setMinWidth(800);
 
+
         //horizontal listener
-        stage.widthProperty().addListener((obs, oldVal, newVal) -> {
-            double scaleX = newVal.doubleValue()/width;
+        stage.getScene().widthProperty().addListener((obs, oldVal, newVal) -> {
+            double scaleX = (newVal.doubleValue()/width);
             scenePane.setScaleX(scaleX);
             scenePane.setTranslateX(scenePane.getTranslateX() + (newVal.doubleValue()-oldVal.doubleValue())/2);
         });
 
         //vertical listener
-        stage.heightProperty().addListener((obs, oldVal, newVal) -> {
-            double scaleY = newVal.doubleValue()/height;
+        stage.getScene().heightProperty().addListener((obs, oldVal, newVal) -> {
+            double scaleY = (newVal.doubleValue()/height);
             scenePane.setScaleY(scaleY);
             scenePane.setTranslateY(scenePane.getTranslateY() + (newVal.doubleValue()-oldVal.doubleValue())/2);
         });
